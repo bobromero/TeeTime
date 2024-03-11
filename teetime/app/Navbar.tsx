2b377b15ad60ead272f10a2bddf1a9bd4193a3b7
Robert Romero <rromero123102@gmail.com>
2024-03-11 18:42:06 -0400

    updated styling and added image

--- teetime/app/Navbar.tsx
+++ teetime/app/Navbar.tsx
@@ -1,15 +1,25 @@
 import Link from 'next/link'
 import React from 'react'
+import { QueryImage } from './PhotoServer'
 
 const Navbar = () => {
   return (
-    <div>
-      TeeTime
-      <Link href='/'> Home </Link>
-      <Link href='/Rules'>  Rules </Link>
-      <Link href='/Courses'>  Courses</Link>
-      <Link href='/Profile'></Link>
-    </div>
+    <div className="grid grid-cols-6">
+      <div className="flex justify-center items-center Nav-grid-item"><img src={QueryImage(1, 3)}
+        height={50}
+        width={50}
+      /></div>
+
+      <Link className="flex justify-center Nav-grid-item" href='/'>TeeTime</Link>
+
+      <Link className="flex justify-center Nav-grid-item" href='/'> Home </Link>
+      <Link className="flex justify-center Nav-grid-item" href='/Rules'>  Rules </Link>
+      <Link className="flex justify-center Nav-grid-item" href='/Courses'>  Courses</Link>
+      <Link className="flex justify-center Nav-grid-item" href='/Profile'>
+
+        Profile
+      </Link>
+    </div >
   )
 }
 
