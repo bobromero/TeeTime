385eeb8b845c79fa494c9b15357b26123dab8652
Robert Romero <rromero123102@gmail.com>
2024-03-11 11:57:05 -0400

    added register page

--- teetime/app/Navbar.tsx
+++ teetime/app/Navbar.tsx
@@ -5,9 +5,10 @@ const Navbar = () => {
   return (
     <div>
       TeeTime
-      <Link href={'/'}> Home </Link>
-      <Link href={'/Rules'}>  Rules </Link>
-      <Link href={'/Courses'}>  Courses</Link>
+      <Link href='/'> Home </Link>
+      <Link href='/Rules'>  Rules </Link>
+      <Link href='/Courses'>  Courses</Link>
+      <Link href='/Profile'></Link>
     </div>
   )
 }
