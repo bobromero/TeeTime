46ad6f3e1e0748f2eb996bdace884dd5ca37817b
Robert Romero <rromero123102@gmail.com>
2024-03-11 11:35:28 -0400

    updated rules page

---
+++ teetime/app/Rules/page.tsx
@@ -0,0 +1,9 @@
+function Rules() {
+  return <div>
+    <h1>Rules Page</h1>
+    <a href="https://www.pga.org/Document-Library/pat-hard-card-01-2020.pdf">Click Here for Rules</a>
+    <h6>Rules that should be followed for submitting scores into the database, proof of incorrect scoring may result in scores not being displayed in course/site records</h6>
+  </div>
+}
+
+export default Rules
