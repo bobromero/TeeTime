f1dfe188ddf7ee0e91883d5bd6f38c3cf45c4846
Robert Romero <rromero123102@gmail.com>
2024-03-11 11:35:11 -0400

    Created and added navbar to the top of the site

---
+++ teetime/app/Navbar.tsx
@@ -0,0 +1,15 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      TeeTime
+      <Link href={'/'}> Home </Link>
+      <Link href={'/Rules'}>  Rules </Link>
+      <Link href={'/Courses'}>  Courses</Link>
+    </div>
+  )
+}
+
+export default Navbar
