274e0ea0f18fe25051378c23977ba99810d6a761
Robert Romero <rromero123102@gmail.com>
2024-03-14 08:53:09 -0400

    created base sidebar

---
+++ teetime/app/CourseSearch/SideBar.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const SideBar = () => {
+  return (
+    <div>
+
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+      </ul>
+    </div>
+  )
+}
+
+export default SideBar
