b9e114c29f31b9fe0d7653ca2be83b965f677d23
Robert Romero <rromero123102@gmail.com>
2024-03-14 08:53:01 -0400

    updated navbar

--- teetime/app/Navbar.tsx
+++ teetime/app/Navbar.tsx
@@ -4,21 +4,20 @@ import { QueryImage } from './PhotoServer'
 
 const Navbar = () => {
   return (
-    <div className="grid grid-cols-6">
-      <div className="flex justify-center items-center Nav-grid-item"><img src={QueryImage(1, 3)}
+    <div className="grid grid-cols-9">
+
+      <Link href='/' className="flex justify-center  items-center Nav-grid-item"><img src={QueryImage(1, 3)}
         height={50}
         width={50}
-      /></div>
-
-      <Link className="flex justify-center Nav-grid-item" href='/'>TeeTime</Link>
-
-      <Link className="flex justify-center Nav-grid-item" href='/'> Home </Link>
+      />TeeTime</Link>
+      <div></div>
+      <div></div>
+      <div></div>
+      <Link className="flex justify-center Nav-grid-item" href='/Courses'>  Search Courses</Link>
+      <div></div>
+      <div></div>
       <Link className="flex justify-center Nav-grid-item" href='/Rules'>  Rules </Link>
-      <Link className="flex justify-center Nav-grid-item" href='/Courses'>  Courses</Link>
-      <Link className="flex justify-center Nav-grid-item" href='/Profile'>
-
-        Profile
-      </Link>
+      <Link className="flex justify-center Nav-grid-item" href='/Profile'>Profile</Link>
     </div >
   )
 }
