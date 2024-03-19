e929814d9fb29fb64403aa01af9453e5ad726ae3
Robert Romero <rromero123102@gmail.com>
2024-03-19 12:41:50 -0400

    updated navbar to use new function

--- teetime/app/Navbar.tsx
+++ teetime/app/Navbar.tsx
@@ -1,12 +1,12 @@
 import Link from 'next/link'
 import React from 'react'
-import { QueryImage, TypeImage } from './PhotoServer'
+import { QueryImage } from './PhotoServer'
 
 const Navbar = () => {
   return (
     <div className="grid grid-cols-9">
 
-      <Link href='/' className="flex justify-center  items-center Nav-grid-item"><img src={QueryImage(TypeImage.Utility, "logo")}
+      <Link href='/' className="flex justify-center  items-center Nav-grid-item"><img src={QueryImage("logo")}
         height={50}
         width={50}
       />TeeTime</Link>
