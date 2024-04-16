fc078106ebb7fce9e12fa3e7c8c0cc42a28d5903
Robert Romero <rromero123102@gmail.com>
2024-04-16 13:00:26 -0400

    added course tiles that populate with data from getcoursetiles

---
+++ teetime/app/CourseSearch/[numSearch]/courseTiles.tsx
@@ -0,0 +1,36 @@
+import Link from 'next/link';
+import React from 'react'
+
+import { TileInformation, GetCourseTiles } from '../../getCourseTiles';
+
+let TileInfo: TileInformation[] = GetCourseTiles();
+
+const Tiles = ({ num }: { num: number }) => {
+  console.log(num);
+  const elements = [];
+  for (let i = 0; i < num; i++) {
+    elements.push(
+      <Link href={'/Courses/' + TileInfo[i].UPID}>
+        <img src='src' />
+        <h1>{TileInfo[i].Name}</h1>
+        <p>{TileInfo[i].description}</p>
+        <p>{TileInfo[i].address}</p>
+
+      </Link>
+    )
+
+  }
+  return elements;
+}
+
+const CourseTiles = ({ params }: { params: { numSearch: number } }) => {
+  return (
+    <div>
+      <h1>Courses</h1>
+      <Tiles num={params.numSearch} ></Tiles>
+
+    </div>
+  )
+}
+
+export default CourseTiles
