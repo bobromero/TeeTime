f4361f408cc2d8384ed9082c897352c51c5d1465
Robert Romero <rromero123102@gmail.com>
2024-04-17 11:59:33 -0400

    Update user page

--- teetime/app/Users/[uid]/User.tsx
+++ teetime/app/Users/[uid]/User.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { getUserInfo, UserInfo } from '../../getUserInfo'
 
 const User = ({ params }: { params: { uid: string } }) => {
+  let Golfer: UserInfo = getUserInfo(params.uid);
   return (
     <div>
       <h1>UserPage for {params.uid}</h1>
-      <p>User name from db lookup</p>
+      <h2>{Golfer.UserName}</h2>
+      <h2>Handicap: {Golfer.Handicap}</h2>
+      <p>{Golfer.Bio}</p>
     </div>
   )
 }
