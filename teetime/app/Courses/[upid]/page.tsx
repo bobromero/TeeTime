77d1374f8f1ff76138db52d561394acd231305da
Robert Romero <rromero123102@gmail.com>
2024-04-17 11:58:41 -0400

    Redid the course page and made it use the courseinfo file

--- teetime/app/Courses/[upid]/page.tsx
+++ teetime/app/Courses/[upid]/page.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { CourseInformation, GetCourseInfo } from '../../getCourseTiles'
 
 const CoursePage = ({ params }: { params: { upid: string } }) => {
+  let courseInfo: CourseInformation = GetCourseInfo(params.upid);
   return (
-    <div>CoursePage of {params.upid}</div>
+    <div>
+      <h1>CoursePage of {courseInfo.UPID}</h1>
+      <h1>{courseInfo.Name}</h1>
+      <h3>Address: {courseInfo.address}</h3>
+      <p>{courseInfo.description}</p>
+      <h1>Number of holes: {courseInfo.NumberOfHoles}</h1>
+      <h1>Yardage: {courseInfo.Yardage}</h1>
+    </div>
   )
 }
 
