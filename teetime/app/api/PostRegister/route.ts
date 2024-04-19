a0528fee76353417e88e9f5c558d7b3dd8136822
Robert Romero <rromero123102@gmail.com>
2024-04-19 19:23:18 -0400

    made it access the db

--- teetime/app/api/PostRegister/route.ts
+++ teetime/app/api/PostRegister/route.ts
@@ -1,14 +1,44 @@
 import { NextRequest, NextResponse } from 'next/server'
-
-import { AddCourseToDB } from '/Users/Robert/TeeTimeApp/teetime/AddCourse'
+import { pool } from '../../Pool';
 import { NextApiRequest } from 'next';
+import { HashPassword } from '../../PasswordManager';
+import { useRouter } from 'next/navigation';
+
+
 
 export async function POST(request: Request) {
-  const body = await request.json();
-  console.log(body);
-  // AddCourseToDB("RobCourse", "53 Thorp st", "nicest golf around", 9, 4579);
+  const requestData = await request.json();
+  // console.log(requestData);
+  let username = requestData['username'];
+  let password = requestData['password'];
+  console.log(username, password)
+
+  RegisterUser(username, password);
+
+  const router = useRouter()
+  router.push('/');
+
   return NextResponse.json({
     status: 201,
     message: 'pog'
   })
+}
+
+async function RegisterUser(username: string, password: string): Promise<string> {
+  try {
+    const client = await pool.connect();
+
+    const upidResult = await client.query("SELECT uuid_generate_v4();");
+    const upid = upidResult.rows[0];
+
+    const InsertGolfer = await client.query("INSERT INTO golfer(golferid, pfplink, bio, handicap, username, password)values('" + upid + "', 'pfplink', 'bio', -1, " + username + ", " + HashPassword(password) + ");");
+
+    client.release();
+    pool.end();
+    return username;
+  } catch (error) {
+    console.error("Error adding data: ", error);
+    throw error;
+  }
+
 }
