40c4403c4e6b07b8139f798ff9d95c951f69783b
Robert Romero <rromero123102@gmail.com>
2024-04-19 19:22:25 -0400

    added remaining fields

--- teetime/app/Users/[uid]/getUserInfo.ts
+++ teetime/app/Users/[uid]/getUserInfo.ts
@@ -1,14 +1,20 @@
 export type UserInfo = {
   UserName: string,
   Handicap: number,
-  Bio: string
+  Bio: string,
+  GolferID: string,
+  pfpLink: string,
+  hashedPW: string
 }
 
 export function getUserInfo(golferId: string): UserInfo {
   let UI: UserInfo = {
     UserName: "Rob",
     Handicap: 7,
-    Bio: "The big man"
+    Bio: "The big man",
+    GolferID: "",
+    pfpLink: "",
+    hashedPW: "123"
   }
   return UI;
 }
