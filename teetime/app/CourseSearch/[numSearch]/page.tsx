d06cfa901e7ef0aed655fa76be3827f4fbf59b9f
Robert Romero <rromero123102@gmail.com>
2024-04-21 12:23:13 -0400

    redid course search page

--- teetime/app/CourseSearch/[numSearch]/page.tsx
+++ teetime/app/CourseSearch/[numSearch]/page.tsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import SideBar from './SideBar'
 import CourseTiles from './courseTiles'
+import Link from 'next/link'
+
 
 const CourseSearch = ({ params }: { params: { numSearch: number } }) => {
-  return (
-    <div>
-      <SideBar />
 
-      <CourseTiles params={params} />
+  return (
+    <div >
+      {/* <SideBar /> */}
+
+      <div className={'grid grid-cols-' + params.numSearch + ' gap-' + params.numSearch}>
+        <CourseTiles params={params} />
+
+      </div>
+      <Link href={'/CourseAdd/'}>Add a course!</Link>
 
     </div>
   )
