097e24dc76cc39a6b4486c6c30f55d91750308d1
Robert Romero <rromero123102@gmail.com>
2024-04-21 12:20:01 -0400

    Course add page

---
+++ teetime/app/CourseAdd/page.tsx
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+import React from 'react'
+import AddCourseForm from './AddCourseForm'
+
+const CourseAdd = () => {
+  return (
+    <div>
+      <h1>Add Course Page</h1>
+      <AddCourseForm />
+
+
+    </div>
+  )
+}
+
+export default CourseAdd
