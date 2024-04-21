fa63645353dcf37cd18fb938763746a546e915e7
Robert Romero <rromero123102@gmail.com>
2024-04-21 12:18:36 -0400

    adding course api route

--- teetime/app/api/AddCourse/route.ts
+++ teetime/app/api/AddCourse/route.ts
@@ -1,13 +1,41 @@
 import { NextRequest, NextResponse } from 'next/server'
-// import { AddCourseToDB } from '/Users/Robert/TeeTimeApp/teetime/AddCourse';
+import { pool } from '../../Pool';
+
+
+async function AddCourseToDB(name: string, address: string, description: string, holes: number, yardage: number): Promise<string> {
+  try {
+    const client = await pool.connect();
+
+    console.log("connected");
+
+    const upidResult = await client.query("SELECT uuid_generate_v4();");
+    const upid: string = upidResult.rows[0].uuid_generate_v4;
+
+    const InsertPlace = await client.query("INSERT INTO place(name, address, description, upid)values('" + name + "', '" + address + "', '" + description + "', '" + upid + "');");
+
+    const InsertCourse = await client.query("INSERT INTO course(numberofholes,yardage,upid)values(" + holes + "," + yardage + ", '" + upid + "');")
+
+    console.log("inserted place and course");
+
+    client.release();
+    return upid;
+  } catch (error) {
+    console.error("Error adding data: ", error);
+    throw error;
+  }
+}
 
 
 export async function POST(request: Request) {
-  const body = await request.json();
-  console.log(body);
-  // AddCourseToDB("RobCourse", "53 Thorp st", "nicest golf around", 9, 4579);
-  return NextResponse.json({
+  const requestData = await request.json();
+  // console.log(requestData);
+  let coursename = requestData['name'];
+  let address = requestData['address'];
+  let description = requestData['description'];
+  let yardage = requestData['yardage'];
+  let numberofholes = requestData['numberofholes'];
+  const upid = await AddCourseToDB(coursename, address, description, numberofholes, yardage);
+  return new Response(upid, {
     status: 201,
-    message: 'pog'
   })
 }
