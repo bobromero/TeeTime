4e6dc9b278b354545fd269f409f8fdea94258c6f
Robert Romero <rromero123102@gmail.com>
2024-04-21 12:18:01 -0400

    implemented compare password

--- teetime/app/PasswordManager.ts
+++ teetime/app/PasswordManager.ts
@@ -6,7 +6,7 @@ export async function HashPassword(password: string): Promise<string> {
   return hash;
 }
 
-export async function ComparePassword(username: string, password: string) {
-  //!get user password and use bcrypt compare method
-  // const isMatch = await bcrypt.compare(password, hashedpassword)
+export async function ComparePassword(dbPassword: string, password: string): Promise<boolean> {
+  const isMatch = await bcrypt.compare(password, dbPassword)
+  return isMatch;
 }
