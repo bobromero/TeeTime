30fbc7323a4a47c4294eb759a1f6afbf2cfdb479
Robert Romero <rromero123102@gmail.com>
2024-04-21 12:18:53 -0400

    getuser api route

--- teetime/app/api/GetUser/route.ts
+++ teetime/app/api/GetUser/route.ts
@@ -1,8 +1,17 @@
 import { NextResponse } from 'next/server'
 
+import { GetUser } from '../../Users/[uid]/GetUser'
+
 export async function GET(request: Request) {
-  return NextResponse.json({
-    message: 'pog'
+  const requestData = await request.json();
+  // console.log(requestData);
+  let username = requestData['username'];
+
+  const User = await GetUser(username);
+
+
+  return new Response('hello next', {
+    status: 201,
   })
 }
 //TODO we can make this return information about the backend possibly
