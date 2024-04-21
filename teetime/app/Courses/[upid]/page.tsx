bac6a902657ef7892ed3678368ceab65fe9bea02
Robert Romero <rromero123102@gmail.com>
2024-04-21 12:20:57 -0400

    updated course page to support update course link

--- teetime/app/Courses/[upid]/page.tsx
+++ teetime/app/Courses/[upid]/page.tsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import { CourseInformation, GetCourseInfo } from '../../CourseSearch/[numSearch]/getCourseTiles'
+import Link from 'next/link';
 
-const CoursePage = ({ params }: { params: { upid: string } }) => {
-  let courseInfo: CourseInformation = GetCourseInfo(params.upid);
+const CoursePage = async ({ params }: { params: { upid: string } }) => {
+  let courseInfo: CourseInformation = await GetCourseInfo(params.upid);
   return (
     <div>
-      <h1>CoursePage of {courseInfo.UPID}</h1>
       <h1>{courseInfo.Name}</h1>
       <h3>Address: {courseInfo.address}</h3>
       <p>{courseInfo.description}</p>
       <h1>Number of holes: {courseInfo.NumberOfHoles}</h1>
       <h1>Yardage: {courseInfo.Yardage}</h1>
+      <Link href={'/UpdateCourse/' + params.upid}>Update Course</Link>
     </div>
   )
 }
